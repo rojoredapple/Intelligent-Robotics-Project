ddd0719bb9aa43318505dd6975eaee1bba14c1b8
brihu.sundararaman@yale.edu <brihu.sundararaman@yale.edu>
2022-12-03 17:17:37 -0500

    cleanup

--- Zombie world/controllers/youbot_controller/remove_box_inside.py
+++
@@ -1,9 +0,0 @@
-def remove_box_inside(boxes):
-    for idx, box in enumerate(boxes):
-        x, y, w, h = box
-        for box2 in boxes:
-            x2, y2, w2, h2 = box2
-            if box != box2:
-                if x2 < x + w and x2 + w2 > x and y2 < y + h and y2 + h2 > y:
-                    boxes.remove(box2)
-    return boxes
